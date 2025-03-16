01b4d88b406896a0fb8d11637cf8f0f03443dd4a
vidhya-balaji <vidhyabalaji.nina@gmail.com>
2025-03-16 22:29:16 +0530

    changes

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,3 +1,3 @@
 import axios from 'axios'
 
-export const axiosi=axios.create({withCredentials:true,baseURL:"https://bagluxebackend.vercel.app"})
+export const axiosi=axios.create({withCredentials:true,baseURL:"https://bagluxesbackend.vercel.app/"})
